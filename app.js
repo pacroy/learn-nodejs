c44fca47cdeb748fd3d938a921dc7d7862efec85
Par <pacroy@users.noreply.github.com>
2022-07-10 16:06:43 +0700

    Add global objects

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+/* Hello world
+================================================================================*/
 function sayHello(name) {
     console.log('Hello ' + name)
 }
 
 sayHello('Eddie')
+
+/* Global objects
+================================================================================*/
+// console
+// setTimeout()
+// clearTimeout()
+// setInterval()
+// clearInterval()
+// The above are belong to the window object
