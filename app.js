cd80faedbf3ed569d08e7a702403504d7afff2fe
Par <pacroy@users.noreply.github.com>
2022-07-10 17:49:02 +0700

    Add Events module

--- app.js
+++ app.js
@@ -87,3 +87,17 @@ fs.readdir('./', function(err, files) {
     if (err) console.log('Error:', err)
     else console.log(files)
 })
+
+console.log(`
+================================================================================
+7. Events Module
+================================================================================`)
+// We get EventEmitter class from the `require('events')`
+const EventEmitter = require('events')
+const emitter = new EventEmitter()
+
+emitter.on('messageLogged', function() {
+    console.log('Listener called')
+})
+
+emitter.emit('messageLogged')
